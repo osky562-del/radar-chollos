78a5b7059cfbcd222f383d1c2790b92b89f07591
osky562-del <osky562@gmail.com>
2026-03-11 23:59:21 +0100

    Refactor buscar function and clean up code
    Updated the buscar function to modify the inner text and removed unnecessary code for loading results.

--- app.js
+++ app.js
@@ -1,47 +1,5 @@
-import {db,collection,getDocs} from "./firebase.js";
-
 function buscar(){
 
-document.getElementById("resultados").innerText="La app funciona correctamente";
+document.getElementById("resultados").innerText="La app funciona";
 
 }
-
-contenedor.innerHTML="Cargando...";
-
-const querySnapshot = await getDocs(collection(db,"chollos"));
-
-let html="";
-
-querySnapshot.forEach((doc)=>{
-
-let d = doc.data();
-
-html += `
-
-<div class="resultado">
-
-Producto: ${d.producto}
-
-<br>
-
-Compra: ${d.precioCompra}€
-
-<br>
-
-Venta: ${d.precioVenta}€
-
-<br>
-
-Beneficio: ${d.beneficio}€
-
-</div>
-
-`;
-
-});
-
-contenedor.innerHTML=html;
-
-}
-
-window.buscar=buscar;
