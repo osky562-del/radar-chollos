c1fc80b82694bc56bc5e5187b453db351e4e22c6
osky562-del <osky562@gmail.com>
2026-03-12 00:06:32 +0100

    Update scanner.js

--- scanner.js
+++ scanner.js
@@ -1,12 +1,31 @@
-const productos = [
+async function escanear(){
+
+console.log("escaneando chollos...")
+
+// ejemplo simulación
+
+let productos = [
 
 "iphone 13",
-"iphone 12",
 "ps5",
 "amazfit gtr 3",
-"airpods pro",
 "apple watch"
 
 ]
 
-module.exports = productos
+productos.forEach(p => {
+
+let compra = Math.floor(Math.random()*50)+40
+let venta = Math.floor(Math.random()*150)+120
+
+let beneficio = venta-compra
+
+if(beneficio > 40){
+
+console.log("CHOLLO:",p,beneficio)
+
+}
+
+})
+
+}
