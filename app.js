7bfba31d583aafbea265b57e20b3d53f46721dcd
osky562-del <osky562@gmail.com>
2026-03-12 00:09:38 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
-function buscar(){
+import { db, collection, getDocs } from "./firebase.js";
 
-document.getElementById("resultados").innerText="La app funciona";
+async function buscar() {
+  const contenedor = document.getElementById("resultados");
+  contenedor.innerHTML = "Cargando...";
 
+  const querySnapshot = await getDocs(collection(db,"chollos"));
+  let html = "";
+
+  querySnapshot.forEach(doc => {
+    const d = doc.data();
+    html += `
+      <div class="resultado">
+        Producto: ${d.producto}<br>
+        Compra: ${d.precioCompra}€<br>
+        Venta: ${d.precioVenta}€<br>
+        Beneficio: ${d.beneficio}€
+      </div>
+    `;
+  });
+
+  contenedor.innerHTML = html;
 }
+
+window.buscar = buscar;
