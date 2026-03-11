2ecdb22f73c5087bdad16a6f140e7f93945eabc7
osky562-del <osky562@gmail.com>
2026-03-12 00:38:05 +0100

    Create firebase.js

---
+++ bot/firebase.js
@@ -0,0 +1,17 @@
+// firebase.js
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "TU_API_KEY",
+  authDomain: "TU_PROYECTO.firebaseapp.com",
+  projectId: "TU_PROYECTO",
+  storageBucket: "TU_PROYECTO.appspot.com",
+  messagingSenderId: "XXXX",
+  appId: "XXXX"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
